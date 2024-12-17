237f73a11144361fdd4a70bebc7c6f2a1bae13bd
f-kazuo <kaz.furui@gmail.com>
2024-12-17 22:53:30 +0900

    Update schedule.js

--- pages/schedule.js
+++ pages/schedule.js
@@ -150,20 +150,16 @@ return (
           </button>
         </Link>
        </li>
-       <li>
         <Link href="/schedule">
           <button className="bg-[#E6B422] hover:bg-[#D4A41B] text-white font-medium px-8 py-2 rounded">
             配達スケジュール
           </button>
         </Link>
-       </li>
-       <li>
         <Link href="/purchase">
           <button className="bg-[#E6B422] hover:bg-[#D4A41B] text-white font-medium px-8 py-2 rounded">
             購入
           </button>
         </Link>
-       </li>
       </div>
     </nav>
 
